880e3cfe18a9ff7c5fc5468957218b29accd2ede
SangaChoi <tkddk92@naver.com>
2019-03-03 01:08:21 +0900

    finish

---
+++ src/main/java/spring/service/aop/advice/TestAspectJ02.java
@@ -0,0 +1,74 @@
+package spring.service.aop.advice;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class TestAspectJ02 {
+	
+	public TestAspectJ02() {
+		System.out.println("::TestAspectJ02 Default Constructor");
+	}
+	
+/*	@Pointcut("execution(* *.getMessage(..))")
+	public void work() {
+		System.out.println("work() pointcut call");
+	}
+*/
+	@Before("execution(* *.getMessage(..))")
+	public void before(JoinPoint joinPoint) {
+		System.out.println("[before LOG]"+getClass()+".before() start.......");
+		System.out.println("[before LOG] targetObject : "+joinPoint.getTarget().getClass().getName());
+		System.out.println("[before LOG] targetObject »£√‚µ… method : "+joinPoint.getSignature().getName());
+		
+		if(joinPoint.getArgs().length!=0) {
+			System.out.println("[before LOG] targetObject call Method args : "+joinPoint.getArgs()[0]);
+		}
+		System.out.println("[before LOG]\"+getClass()+\".before() end");
+	}
+	
+	@AfterReturning(pointcut="within(spring.service.aop..*)", returning="returnValue")
+	public void afterReturning(JoinPoint joinPoint, Object returnValue) {
+		System.out.println("[after LOG]"+getClass()+".afterReturning() start");
+		System.out.println("[after LOG] ≈∏∞Ÿ ∞¥√º : "+joinPoint.getTarget().getClass().getName());
+		System.out.println("[after LOG] ≈∏∞Ÿ ∞¥√º¿« »£√‚µ» method : "+joinPoint.getSignature().getName());
+		System.out.println("[after LOG] ≈∏∞Ÿ ∞¥√º¿« »£√‚»ƒ return value : "+returnValue);
+		System.out.println("[after LOG]"+getClass()+".afterReturning() end");
+	}
+	
+	@Around("execution(* spring.service.aop.*.*(..))")
+	public Object invoke(ProceedingJoinPoint joinPoint) throws Throwable{
+		System.out.println("[Around before]"+getClass()+".invoke() start.........");
+		System.out.println("[Around before] ≈∏∞Ÿ ∞¥√º : "+joinPoint.getTarget().getClass().getName());
+		System.out.println("[Around before] ≈∏∞Ÿ ∞¥√º¿« »£√‚ µ… method : "+joinPoint.getSignature().getName());
+		if(joinPoint.getArgs().length!=0) {
+			System.out.println("[Around before] ≈∏∞Ÿ ∞¥√º¿« »£√‚ «“ methodø° ¿¸¥ﬁµ«¥¬ ¿Œ¿⁄ : "+joinPoint.getArgs()[0]);
+		}
+		
+		Object obj=joinPoint.proceed();
+		
+		System.out.println("[Around after] ≈∏∞Ÿ ∞¥√º¿« »£√‚»ƒ return value : "+obj);
+		System.out.println("[Around after]"+getClass()+"invoke() end............");
+		
+		return obj;
+	}
+	
+
+	@AfterThrowing(pointcut="execution(public * *(..))", throwing="throwable")
+	public void afterThrowing(JoinPoint joinPoint, Throwable throwable) {
+		
+		System.out.println("[exception]"+getClass()+".afterThrowing() start........");
+		System.out.println("[exception] ≈∏∞Ÿ ∞¥√º : "+joinPoint.getTarget().getClass().getName());
+		System.out.println("[exception] ≈∏∞Ÿ ∞¥√º¿« »£√‚µ» method : "+joinPoint.getSignature().getName());
+		System.out.println("[exception] Exception πﬂª˝");
+		System.out.println("[exception] Exception Message : "+throwable.getMessage());
+		System.out.println("[exception]"+getClass()+".afterThrowing() end..........");
+	}
+
+}
